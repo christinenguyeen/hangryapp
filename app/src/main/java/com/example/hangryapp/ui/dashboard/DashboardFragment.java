2f6c706dd59033efedb560b75a6c51fb95b354b0
Tomas Doherty <jacksondohertypro@gmail.com>
2021-03-18 15:08:47 -0400

    more stuff

--- app/src/main/java/com/example/hangryapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/hangryapp/ui/dashboard/DashboardFragment.java
@@ -11,25 +11,34 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hangryapp.R;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        /*dashboardViewModel =
+                new ViewModelProvider(this).get(DashboardViewModel.class);*/
+        //final TextView textView = root.findViewById(R.id.text_dashboard);
+        /*dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        recyclerView = root.findViewById(R.id.preference_list);
+        PreferenceListAdapter listAdapter = new PreferenceListAdapter(getContext());
+        recyclerView.setAdapter(listAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
         return root;
     }
 }
