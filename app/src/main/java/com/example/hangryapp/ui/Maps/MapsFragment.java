df756b92bc2835d98f8bed407d17b9bb22ebc2d7
slp20028 <steven.pascua98@gmail.com>
2021-03-25 16:07:45 -0400

    Google Maps functionally

--- app/src/main/java/com/example/hangryapp/ui/Maps/MapsFragment.java
+++ app/src/main/java/com/example/hangryapp/ui/Maps/MapsFragment.java
@@ -2,24 +2,36 @@ package com.example.hangryapp.ui.Maps;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.hangryapp.R;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
 public class MapsFragment extends Fragment {
-
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
+        private LocationManager lm;
 
         /**
          * Manipulates the map once available.
@@ -32,10 +44,23 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            LatLng currentLoc = new LatLng(38.8298,-77.3074);
+            CameraPosition cameraPosition = new CameraPosition.Builder().
+                    target(currentLoc).
+                    tilt(60).
+                    zoom(17).
+                    bearing(0).
+                    build();
+            googleMap.setBuildingsEnabled(true);
+            googleMap.addMarker(new MarkerOptions().position(currentLoc).title("Current Location"));
+           /* googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
+            googleMap.moveCamera(CameraUpdateFactory.zoomTo(17));*/
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
         }
+
+
     };
 
     @Nullable
@@ -44,6 +69,7 @@ public class MapsFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_maps, container, false);
+
     }
 
     @Override
