5d598380d034b5b391105e307c678ae29cb57a70
Christine Nguyen <christinenguyen@Christines-MacBook-Pro-2.local>
2021-04-12 23:05:14 -0400

    final food decider

--- app/src/main/java/com/example/hangryapp/data/FoodDecider.java
+++ app/src/main/java/com/example/hangryapp/data/FoodDecider.java
@@ -1,4 +1,15 @@
 package com.example.hangryapp.data;
 
+import com.example.hangryapp.data.PreferenceData;
+
+import java.util.ArrayList;
+
 public class FoodDecider {
+    private ArrayList<PreferenceData> preferenceList;
+
+    public PreferenceData getPreference() {
+        int random = (int)(Math.random()*this.preferenceList.size());
+
+        return this.preferenceList.get(random);
+    }
 }
