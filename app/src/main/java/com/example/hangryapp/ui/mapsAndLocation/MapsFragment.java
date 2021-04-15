aee430de7adc55d8f95c246f6a62ce0b8df4caee
slp20028 <steven.pascua98@gmail.com>
2021-04-15 13:24:52 -0400

    Added Location Services

--- app/src/main/java/com/example/hangryapp/ui/mapsAndLocation/MapsFragment.java
+++ app/src/main/java/com/example/hangryapp/ui/mapsAndLocation/MapsFragment.java
@@ -2,9 +2,20 @@ package com.example.hangryapp.ui.mapsAndLocation;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +30,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsFragment extends Fragment {
+    Context mContext;
+    LocationManager locationManager;
+    Location lastKnownLocation;
+    Activity activity;
+    String provider;
+    private static final int REQUEST_LOCATION = 123;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -33,7 +50,17 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng currentLoc = new LatLng(38.8298,-77.3074);
+            LatLng currentLoc;
+            if (lastKnownLocation != null)
+            {
+                currentLoc = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                Log.d("GMAP","USERLOCATION");
+            }
+            else {
+                currentLoc = new LatLng(38.8298, -77.3074);
+                Log.d("GMAP","ISGMU");
+            }
+            Log.d("GMAP","provider: " + provider);
             CameraPosition cameraPosition = new CameraPosition.Builder().
                     target(currentLoc).
                     tilt(60).
@@ -53,6 +80,29 @@ public class MapsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        mContext = getActivity().getApplicationContext();
+        activity = getActivity();
+        locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), true);
+        Log.d("GMAP", "provider is null: " + String.valueOf(provider == null));
+
+        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions( activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION}, REQUEST_LOCATION);
+
+        }
+        if (provider == null)
+        {
+            //TODO Display this: The Hangry App could not get location permissions! Your current location will default to George Mason University campus.
+            return inflater.inflate(R.layout.fragment_maps, container, false);
+        }
+        lastKnownLocation = locationManager.getLastKnownLocation(provider);
         return inflater.inflate(R.layout.fragment_maps, container, false);
     }
 
@@ -65,4 +115,5 @@ public class MapsFragment extends Fragment {
             mapFragment.getMapAsync(callback);
         }
     }
+
 }
